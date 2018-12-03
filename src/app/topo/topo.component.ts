eaea01f0ede9580853a612382902ea63009fa830
unknown <dmsdaniel11@gmail.com>
2018-12-03 21:10:47 -0200

    This is a new commit for what I originally planned to be amended

---
+++ src/app/topo/topo.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core'
+
+@Component({
+	// selector: '[app-topo]',
+	selector: 'app-topo',
+	templateUrl: './topo.component.html',
+	// template: '<p>Component Topo</p>'
+	// template: `
+	// 	<p>Esse é o component topo</p>
+	// `
+
+	// styles: [' .exemplo { color: red } ']
+	styleUrls: ['./topo.component.css']
+
+})
+
+export class TopoComponent {}
