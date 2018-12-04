28524ac3856490f8687093eafb3ae4bdfaa1ca27
unknown <dmsdaniel11@gmail.com>
2018-12-04 01:47:44 -0200

    frases-mock criado

--- src/app/painel/painel.component.ts
+++ src/app/painel/painel.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Frase } from '../shared/frase.model'
+import { FRASES } from './frases-mock'
+
 @Component({
   selector: 'app-painel',
   templateUrl: './painel.component.html',
@@ -7,9 +10,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PainelComponent implements OnInit {
 
-  constructor() { }
+	public frases: Frase[] = FRASES
+	public instrucao: string = 'Traduza a frase:'
 
-  ngOnInit() {
-  }
+  	constructor() { console.log(this.frases) }
+
+  	ngOnInit() {
+  	}
 
 }
