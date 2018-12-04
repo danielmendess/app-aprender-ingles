a9305479d39089ca4ce9c0821f3206c1f3394528
unknown <dmsdaniel11@gmail.com>
2018-12-03 22:24:14 -0200

    template do component topo finalizado

--- src/app/topo/topo.component.ts
+++ src/app/topo/topo.component.ts
@@ -1,17 +1,9 @@
 import { Component } from '@angular/core'
 
 @Component({
-	// selector: '[app-topo]',
 	selector: 'app-topo',
 	templateUrl: './topo.component.html',
-	// template: '<p>Component Topo</p>'
-	// template: `
-	// 	<p>Esse é o component topo</p>
-	// `
-
-	// styles: [' .exemplo { color: red } ']
-	styleUrls: ['./topo.component.css']
-
+	styleUrls: ['./topo.component.css'],
 })
 
 export class TopoComponent {}
